3773c12ec8df3e6a6beea5ec93be0b3c1309b919
zshi <zhenrongshi@Zhenrongs-MacBook-Pro.local>
2018-07-17 20:20:35 -0400

    constructor pattern

---
+++ Creational/main.js
@@ -0,0 +1,10 @@
+var Task = require('./taskClass');
+
+var task1 = new Task('Create a sample task 1');
+var task2 = new Task('Create a sample task 2');
+
+task1.save();
+task2.save();
+
+task1.complete();
+task2.complete();
---
+++ Creational/taskClass.js
@@ -0,0 +1,19 @@
+'use stric';
+
+class Task{
+    constructor(name){
+        this.name = name;
+        this.completed = false;
+    };
+
+    complete(){
+        console.log('Completing task: '+this.name);
+        this.completed = true;
+    };
+
+    save(){
+        console.log('Saving task: '+this.name);
+    };
+};
+
+module.exports = Task;
